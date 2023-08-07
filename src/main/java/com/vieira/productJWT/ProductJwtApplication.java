af9bff894cfc49294a3404104a59845cb00968a9
wmvieira21 <williammartinsvieira334@gmail.com>
2023-08-07 15:52:43 -0300

    project initialization

---
+++ src/main/java/com/vieira/productJWT/ProductJwtApplication.java
@@ -0,0 +1,13 @@
+package com.vieira.productJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vieira/productJWT/ProductJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vieira.productJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
